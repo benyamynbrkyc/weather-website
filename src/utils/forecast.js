486433604e887b1ab5ae35ddecbd6b73e660ed93
benyamynbrkyc <benjamin.brkic@gmail.com>
2019-12-15 22:27:05 +0100

    Added console.log for body.currently

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,9 +10,11 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location! / Coordinate error', undefined);
     } else {
+      console.log(body.currently);
+
       callback(
         undefined,
-        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} celsius.\nFeels like ${body.currently.apparentTemperature} celsius. There is ${body.currently.precipProbability} chance of rain.\nAtmospheric pressure is ${body.currently.pressure}psi`
+        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} celsius.\nFeels like ${body.currently.apparentTemperature} celsius. There is ${body.currently.precipProbability} chance of rain.\nAtmospheric pressure is ${body.currently.pressure}`
       );
     }
   });
